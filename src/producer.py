916bc2792bc8d689ea7d84eff415d60f6aa630a8
yaroslav-ddt <88374855+yaroslav-ddt@users.noreply.github.com>
2022-01-21 21:10:33 +0100

    Update producer.py

--- src/producer.py
+++ src/producer.py
@@ -4,7 +4,7 @@ from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers='kafka.test.yy:9093',
                          value_serializer=lambda x:
                          dumps(x).encode('utf-8'))
-for e in range(1000):
+for e in range(10):
     data = {'number' : e}
     producer.send('numtest', value=data)
     print('message sent')
