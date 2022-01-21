e33b44a1242013325269498b2538193d9267df13
yaroslav-ddt <88374855+yaroslav-ddt@users.noreply.github.com>
2022-01-21 18:14:46 +0100

    Update consumer.py

--- src/consumer.py
+++ src/consumer.py
@@ -2,7 +2,7 @@ from kafka import KafkaConsumer
 from json import loads
 consumer = KafkaConsumer(
     'numtest',
-     bootstrap_servers=['localhost:9092'],
+     bootstrap_servers=['kafka.test.yy:9093'],
      auto_offset_reset='earliest',
      enable_auto_commit=True,
      group_id='my-group',
